1fd7813aec19cdf50010addd72249e864391c395
kwangjin <kwangjin5468@naver.com>
2023-03-19 20:38:54 +0900

    profile1 ui 만들었다.

---
+++ app/src/main/java/com/example/graduationwork/Profile1.java
@@ -0,0 +1,18 @@
+package com.example.graduationwork;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Profile1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile1);
+    }
+}
