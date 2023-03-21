1822c9dc4c01f1e1b83a40dabd6e6c92fe584bd6
Sungho <ddooochii@gmail.com>
2023-03-21 19:09:45 +0900

    메인페이지 밑에 수정하면댐

--- app/src/main/java/com/example/graduationwork/MainActivity.java
+++ app/src/main/java/com/example/graduationwork/MainActivity.java
@@ -14,15 +14,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Display display = getWindowManager().getDefaultDisplay();
-        DisplayMetrics outMetrics = new DisplayMetrics ();
-        display.getMetrics(outMetrics);
-
-        float density = getResources().getDisplayMetrics().density;
-        float dpHeight = outMetrics.heightPixels / density;
-        float dpWidth = outMetrics.widthPixels / density;
-
-        //dp별 layout 별도 적용
-        Log.d("Device dp","dpHeight : : "+dpHeight+"  dpWidth : "+dpWidth+"  density : "+density);
     }
 }
