d2c35da7aa5c8a77ee7a8fbce17daba98bbd0acf
kwangjin <kwangjin5468@naver.com>
2023-04-02 22:11:11 +0900

    3개 ui 화면 추가

---
+++ app/src/main/java/com/example/graduationwork/StylePage.java
@@ -0,0 +1,14 @@
+package com.example.graduationwork;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StylePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.stylepage);
+
+    }
+}
