a4d09619018cec9ed79625549598f85816e9006f
kwangjin <kwangjin5468@naver.com>
2023-04-03 21:59:40 +0900

    뷰바인딩 기능 추가

--- app/src/main/java/com/example/graduationwork/StylePage.java
+++ app/src/main/java/com/example/graduationwork/StylePage.java
@@ -1,14 +1,35 @@
 package com.example.graduationwork;
 
+
 import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.graduationwork.databinding.StylepageBinding;
+
+
 
 public class StylePage extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stylepage);
+        // 뷰바인딩 객체 획득
+        StylepageBinding binding = StylepageBinding.inflate(getLayoutInflater());
+        // 액티비티 화면 출력
+        setContentView(binding.getRoot());
 
+        binding.styleBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.styleBack.setVisibility(View.VISIBLE);
+                // 서브 액티비티로 전환
+                setContentView(R.layout.profile_re);
+            }
+        });
     }
 }
+
+
