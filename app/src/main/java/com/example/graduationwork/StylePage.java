6da14449a5d683f03fe6b7377cdf9175c939b1d2
kwangjin <kwangjin5468@naver.com>
2023-04-04 22:38:47 +0900

    뷰바인딩 기능 추가2

--- app/src/main/java/com/example/graduationwork/StylePage.java
+++ app/src/main/java/com/example/graduationwork/StylePage.java
@@ -1,34 +1,65 @@
 package com.example.graduationwork;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.graduationwork.databinding.CommentspageBinding;
+import com.example.graduationwork.databinding.ProfileReBinding;
 import com.example.graduationwork.databinding.StylepageBinding;
 
 
-
 public class StylePage extends AppCompatActivity {
-
+    private StylepageBinding binding;
+    private ProfileReBinding secondScreenBinding;
+    private CommentspageBinding thirdScreenBinding;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.stylepage);
-        // 뷰바인딩 객체 획득
-        StylepageBinding binding = StylepageBinding.inflate(getLayoutInflater());
-        // 액티비티 화면 출력
+        binding = StylepageBinding.inflate(getLayoutInflater());
+        secondScreenBinding = ProfileReBinding.inflate(getLayoutInflater());
+        thirdScreenBinding = CommentspageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
 
         binding.styleBack.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                binding.styleBack.setVisibility(View.VISIBLE);
-                // 서브 액티비티로 전환
-                setContentView(R.layout.profile_re);
+            public void onClick(View v) {
+                setContentView(secondScreenBinding.getRoot());
             }
         });
+
+        binding.StylePageCommentsEx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StylePage.this, CommentsPage.class);
+                startActivity(intent);
+            }
+        });
+
+        secondScreenBinding.profileReCaStyle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StylePage.this, StylePage.class);
+                startActivity(intent);
+            }
+        });
+
+        thirdScreenBinding.commentsBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(binding.getRoot());
+            }
+        });
+
+
+
+
     }
 }
 
