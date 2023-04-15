feb8c82a8688d80141b5a22c9c3e6680d28d6593
kwangjin <kwangjin5468@naver.com>
2023-04-15 23:10:11 +0900

    4/15

--- app/src/main/java/com/example/graduationwork/Profile_re.java
+++ app/src/main/java/com/example/graduationwork/Profile_re.java
@@ -5,27 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.graduationwork.databinding.ProfileReBinding;
 
 
 
 public class Profile_re extends AppCompatActivity {
 
-    private ProfileReBinding binding;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ProfileReBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        binding.profileReCaStyle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Profile_re.this, StylePage.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.profile_re);
     }
 }
